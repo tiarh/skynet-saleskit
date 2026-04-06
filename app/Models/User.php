c70b24eb0fe6df03cafe18c1f92f7aeceb1679cb
Fairus In'am Pratama <fairusinam08@gmail.com>
2026-04-06 16:24:51 +0700

    fix: restore missing imports in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,14 @@
 
 namespace App\Models;
 
+use Database\Factories\UserFactory;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'username', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
